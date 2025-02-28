168f596415f702092d9773b59ef658595fd89d42
P040631 <>
2025-02-28 23:31:06 +0900

    fix base_url

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./page/Main";
+import Test from "./page/Test";
+import TestResult from "./page/TestResult";
+
+// export const base_url = "http://localhost:5173";
+export const base_url = "http://mytestweburl.com"; 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/:testParam" element={<Test />} />
+        <Route path="/:testParam/result/:resultParam" element={<TestResult />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+// http://localhost:5173/personalColor/result/aa
