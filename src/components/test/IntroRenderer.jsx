6d49cc1972bd4d5fa7e144eb4809281fc3e07f57
P040631 <>
2025-03-01 17:29:17 +0900

    퀴즈 페이지 만들기 1

--- src/components/test/IntroRenderer.jsx
+++ src/components/test/IntroRenderer.jsx
@@ -8,11 +8,28 @@ import Loading from "./Loading";
 function IntroRenderer({currentTest}) {
 
     const [mode, setMode] = useState("intro");
+    const [mbtiScore, setMbtiScore] = useState({
+        E: 0,
+        I: 0,
+        S: 0,
+        N: 0,
+        T: 0,
+        F: 0,
+        J: 0,
+        P: 0,
+    });
+
+
 
     if (mode === "intro") {
         return <Intro info={currentTest?.info} setMode={setMode}/>;
     } else if (mode === "quiz") {
-        return <Quiz />;
+        return <Quiz 
+            questions={currentTest?.questions}
+            mbtiScore={mbtiScore}
+            setMbtiScore={setMbtiScore}
+            setMode={setMode}
+         />;
     } else if (mode === "loading") {
         return <Loading />;
     } else {
--- src/components/test/Quiz.jsx
+++ src/components/test/Quiz.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
-function Quiz() {
-  return (
-    <div>Quiz</div>
-  );
+function Quiz({questions, mbtiScore, setMbtiScore, setMode}) {
+
+    const [questionNum, setQuestionNum] = React.useState(0);
+    const onOptionClick = (option) => {
+        setQuestionNum(questionNum + 1);
+    };
+
+    return ( 
+        <div>
+            <h3>{questions[questionNum]?.question}</h3>
+            <button onClick={onOptionClick}>{questions[questionNum]?.answers[0]?.content}</button>
+            <button onClick={onOptionClick}>{questions[questionNum]?.answers[1]?.content}</button>
+            <div>프로그래스 바</div>
+        </div>
+    );
 }
 
 export default Quiz;
