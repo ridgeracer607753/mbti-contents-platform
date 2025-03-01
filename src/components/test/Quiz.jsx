3a019ab5d402dcce76806be105efb55f12646141
P040631 <>
2025-03-01 18:31:31 +0900

    퀴즈 페이지 만들기 2

--- src/components/test/Quiz.jsx
+++ src/components/test/Quiz.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
 function Quiz({questions, mbtiScore, setMbtiScore, setMode}) {
 
-    const [questionNum, setQuestionNum] = React.useState(0);
-    const onOptionClick = (option) => {
+    const [questionNum, setQuestionNum] = useState(0);
+    const onOptionClick = (type) => () => {
+        mbtiScore[type] += 1;
+        setMbtiScore({...mbtiScore});
+        console.log(type);
+        // Update the mbtiScore based on the type
+       
         setQuestionNum(questionNum + 1);
     };
+
+    useEffect(() => {
+        if (questionNum >= questions.length) {
+            setMode("loading");
+        }
+    },[questionNum,questions.length,setMode]);
 
     return ( 
         <div>
             <h3>{questions[questionNum]?.question}</h3>
-            <button onClick={onOptionClick}>{questions[questionNum]?.answers[0]?.content}</button>
-            <button onClick={onOptionClick}>{questions[questionNum]?.answers[1]?.content}</button>
+            <button onClick={onOptionClick(questions[questionNum]?.answers[0]?.type)}>{questions[questionNum]?.answers[0]?.content}</button>
+            <button onClick={onOptionClick(questions[questionNum]?.answers[1]?.type)}>{questions[questionNum]?.answers[1]?.content}</button>
             <div>프로그래스 바</div>
         </div>
     );
