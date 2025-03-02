fa2b025958b3d0a7af99a53e7045b50c0c1c82ec
P040631 <>
2025-03-03 01:31:14 +0900

    결과 페이지 기본 버튼 그룹 만들기

--- src/components/testResult/ResultButtonGroup.jsx
+++ src/components/testResult/ResultButtonGroup.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import CopyToClipboard from 'react-copy-to-clipboard'
+import { LinkOutlined,ReloadOutlined,HomeOutlined } from '@ant-design/icons'
+import { base_url } from '../../App'
+import { useNavigate } from 'react-router-dom'
+import style from './resultButtonGroup.module.css'
 
-function ResultButtonGroup() {
+const ResultButtonGroup = ({testParam, resultParam,renderTestInfo}) => {
+
+    const navigate = useNavigate();
+    const onClickHome = () => {
+        navigate("/");
+    }
+    const onClickReload = () => {
+        navigate(`/${testParam}`);
+    }
+
+
   return (
-    <div>ResultButtonGroup</div>
+    <div className={style.mainDiv}>
+        <div className={style.upperDiv}>
+        <CopyToClipboard text={`${base_url}/${testParam}/result/${resultParam}`}>
+                <button className={style.upperButton}
+                onClick={() => alert('URL이 복사되었습니다.')}>
+                    <LinkOutlined />
+                    &nbsp; 링크복사
+                </button>
+            </CopyToClipboard>
+           
+            <button className={style.upperButton}
+            onClick={onClickReload}>
+                <ReloadOutlined />
+                &nbsp; 다시하기
+            </button>
+        </div>
+        <div className={style.bottomDiv}>
+            <button className={style.bottomButton}
+            onClick={onClickHome}>
+            <HomeOutlined />
+            &nbsp; 다른 테스트 하러 가기
+            </button>
+        </div>
+    </div>
   )
 }
 
