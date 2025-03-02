d10c55ca309bc73d7f53ff8b81ac7668ac9c0496
P040631 <>
2025-03-02 22:48:07 +0900

    SNS 공유 버튼 그룹 만들기

--- src/components/testResult/ShareButtonGroup.jsx
+++ src/components/testResult/ShareButtonGroup.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { FacebookIcon, FacebookShareButton, TwitterShareButton, XIcon } from 'react-share'
+import { base_url } from '../../App'
 
-function ShareButtonGroup() {
+const ShareButtonGroup = ({testParam, resultParam,renderTestInfo}) => {
   return (
-    <div>ShareButtonGroup</div>
+    <div>
+        <h3>공유하기</h3>
+        <div>
+            <FacebookShareButton
+            url={`${base_url}/${testParam}/result/${resultParam}`}
+            hashtag={`#${renderTestInfo?.info?.mainTitle}`}
+            quote='나만의 테스트 결과를 확인해보세요!'>
+                <FacebookIcon size={48} round={true} />
+            </FacebookShareButton>
+            <TwitterShareButton
+            title={renderTestInfo?.info?.mainTitle}
+            url={`${base_url}/${testParam}/result/${resultParam}`}
+            hashtags={[renderTestInfo?.info?.mainTitle]}
+            >
+                <XIcon size={48} round={true} />
+            </TwitterShareButton>
+            
+        </div>
+    </div>
   )
 }
 
